cadac2ef95fd3c965f11ea7ea0be74de43f06053
Dale Dahlenburg <dale.19@icloud.com>
2023-01-09 20:12:57 +1000

    crud added

--- index.js
+++ index.js
@@ -70,14 +70,45 @@ app.get('/entries/:id', async (req, res) => {
     } 
 })
 
+// Update
+app.put('/entries/:id', async (req, res) => {
+    const { category, content } = req.body
+    const newEntry = { category, content }
+
+    try {
+       const entry = await EntryModel.findByIdAndUpdate(req.params.id, newEntry, { returnDocument: 'after'})
+        if (entry) {
+            res.send(entry)
+        } else {
+            res.status(404).send({ error: 'Entry not found' })
+        } 
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    } 
+})
+
+// Delete
+app.delete('/entries/:id', async (req, res) => {
+    try {
+       const entry = await EntryModel.findByIdAndDelete(req.params.id)
+        if (entry) {
+            res.sendStatus(204)
+        } else {
+            res.status(404).send({ error: 'Entry not found' })
+        } 
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    } 
+})
+
 app.post('/entries', async (req, res) => {
     try {
         // 1. create new entry object with values passed in from the request
         const { category, content } = req.body
         const newEntry = { category, content}
-        // const newEntry = { category: req.body.category, content: req.body.content }
         // 2. push the new entry to the entries array
-        // entries.push(newEntry)
         const insertedEntry = await EntryModel.create(newEntry)
         // 3. send the new entry with 201 status
         res.status(201).send(insertedEntry)
@@ -92,10 +123,8 @@ app.post('/categories', async (req, res) => {
         // 1. create new entry object with values passed in from the request
         const { name } = req.body
         const newCategory = { name }
-        // const newEntry = { category: req.body.category, content: req.body.content }
         // 2. push the new entry to the entries array
-        // entries.push(newEntry)
-        const insertedCategory = await CatgeoryModel.create(newCategory)
+        const insertedCategory = await CategoryModel.create(newCategory)
         // 3. send the new entry with 201 status
         res.status(201).send(insertedCategory)
     }
